826322f95a119f0792f1b7bacf20ae737c74f2b4
kretoffer <20kretovartem000@gmail.com>
2025-09-06 14:43:33 +0300

    add answer agent

--- problem-solver/py/modules/testing_module/testing_module.py
+++ problem-solver/py/modules/testing_module/testing_module.py
@@ -1,11 +1,13 @@
 from sc_kpm import ScModule
 from .testing_agent import GetNextQuestionAgent
 from .finish_test_agent import FinishTestAgent
+from .answer_agent import AnswerAgent
 
 
 class TestingModule(ScModule):
     def __init__(self):
         super().__init__(
             GetNextQuestionAgent(),
+            AnswerAgent()
             #FinishTestAgent()
         )
