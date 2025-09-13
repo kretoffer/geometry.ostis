4eb5405b173950ea80fb6bf83639c9d77971f0da
jstrg7 <sashchekaea@gmail.com>
2025-09-13 20:29:41 +0300

    updating module

--- problem-solver/py/modules/recommendations_module/recommendations_module.py
+++ problem-solver/py/modules/recommendations_module/recommendations_module.py
@@ -2,12 +2,13 @@ from sc_kpm import ScModule
 from .form_test_recommendations_for_user_agent import FormTestRecommendationsForUserAgent
 from .form_task_recommendations_for_user_agent import FormTaskRecommendationsForUserAgent
 from .form_theme_recommendations_for_user_to_study_agent import FormThemeRecommendationsForUserToStudyAgent
-
+from .form_theme_recommendations_for_user_to_solve_test_or_problem_agent import FormThemeRecommendationsForUserToSolveTestOrProblemAgent
 
 class RecommendationsModule(ScModule):
     def __init__(self):
         super().__init__(
             FormTestRecommendationsForUserAgent(),
             FormTaskRecommendationsForUserAgent(),
-            FormThemeRecommendationsForUserToStudyAgent()
+            FormThemeRecommendationsForUserToStudyAgent(),
+            FormThemeRecommendationsForUserToSolveTestOrProblemAgent()
         )
