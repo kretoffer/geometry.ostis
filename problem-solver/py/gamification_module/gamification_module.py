065d527616e734926a69b6a1457af2ef7e64f0c0
jstrg7 <sashchekaea@gmail.com>
2025-09-15 19:53:53 +0300

    GamificationModule inicialized

---
+++ problem-solver/py/gamification_module/gamification_module.py
@@ -0,0 +1,6 @@
+from sc_kpm import ScModule
+
+
+class GamificationModule(ScModule):
+    def __init__(self):
+        super().__init__()
---
+++ problem-solver/py/gamification_module/set_achievement_for_streak_agent.py
@@ -0,0 +1,51 @@
+import logging
+from typing import List
+from sc_client.models import ScAddr, ScTemplate, ScSet
+from sc_client.constants import sc_type
+from sc_client.client import search_by_template, generate_by_template, delete_elements
+
+from sc_kpm import ScAgentClassic, ScResult
+from sc_kpm.sc_sets import ScSet
+from sc_kpm.utils import (
+    generate_connector,
+    generate_node,
+    get_link_content
+)
+from sc_kpm.utils.action_utils import (
+    finish_action_with_status,
+    get_action_arguments
+)
+from sc_kpm import ScKeynodes
+
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s | %(name)s | %(message)s", datefmt="[%d-%b-%y %H:%M:%S]"
+)
+
+
+class SetAchievementForStreakAgent(ScAgentClassic):
+
+
+    def __init__(self):
+        super().__init__("action_set_achievement_for_streak")
+    
+    def on_event(self, event_element: ScAddr, event_edge: ScAddr, action_element: ScAddr) -> ScResult:
+        result = self.run(action_element)
+        is_successful = result == ScResult.OK
+        finish_action_with_status(action_element, is_successful)
+        self.logger.info("SetAchievementForStreakAgent finished %s",
+                         "successfully" if is_successful else "unsuccessfully")
+        return result
+
+
+    def run(self, action_node: ScAddr) -> ScResult:
+        self.logger.info("SetAchievementForStreakAgent started")
+
+        user = get_action_arguments(action_node, 1)
+
+
+        # TODO
+
+        return ScResult.OK
+
+
