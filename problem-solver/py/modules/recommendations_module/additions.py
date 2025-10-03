4dcf9eaff1c350a8238b97aea6ac4f98505af8dd
kretoffer <20kretovartem000@gmail.com>
2025-10-03 22:29:37 +0300

    fix bug with not addig not studied themes to recommendations

--- problem-solver/py/modules/recommendations_module/additions.py
+++ problem-solver/py/modules/recommendations_module/additions.py
@@ -44,24 +44,21 @@ def get_all_stidied_themes(user: ScAddr) -> list[ScAddr]:
 
 def get_all_not_stidied_themes(user: ScAddr) -> list[ScAddr]:
     templ = ScTemplate()
-    templ.quintuple(
-        user,
-        sc_type.VAR_COMMON_ARC,
-        (sc_type.VAR_NODE, "_not_stidied_themes_set"),
-        sc_type.VAR_PERM_POS_ARC,
-        ScKeynodes.resolve("nrel_not_studied_themes", sc_type.CONST_NODE_NON_ROLE)
-    )
     templ.triple(
-        "_not_stidied_themes_set",
+        ScKeynodes.resolve("concept_theme", sc_type.VAR_NODE_CLASS),
         sc_type.VAR_PERM_POS_ARC,
         (sc_type.VAR_NODE, "_theme")
     )
 
+    studied_themes = get_all_stidied_themes(user)
+
     search_results = search_by_template(templ)
     themes = []
-    if search_results:
-        for result in search_results:
-            themes.append(result.get("_theme"))
+    for result in search_results:
+        theme = result.get("_theme")
+        if theme in studied_themes:
+            continue
+        themes.append(theme)
 
     return themes
      
